e372d9d639dfaf0f3cc1bb0cbc7fd14f3b6a7c1b
Maddy Scopellite <mds>
2023-04-11 08:15:32 -0400

    week 10

---
+++ week_10/button.js
@@ -0,0 +1,21 @@
+
+
+
+
+// const myButton = document.querySelector("container");
+// let theMain = document.getElementById("theMessage");
+// let theP = document.querySelector("p")
+
+// when user clicks button trigger potato function
+myButton.addEventListener('click', show);
+
+function show(){
+    let image = document.getElementById("image");
+ 
+            image.src =
+"https://media-be.chewy.com/wp-content/uploads/2021/04/15160416/Border-Collie_Featured-Image-1024x615.jpg"
+ 
+            document.getElementById("btnID")
+                    .style.display = "none";
+}
+
